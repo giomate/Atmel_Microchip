413f0a4da27431bde2767120efafb3b90a3bf937
giomate <giovannymateus@gmx.de>
2021-03-26 07:38:36 +0100

    avr128db first commit

---
+++ LMX_AVR128DB_LIB/LMX_AVR128DB_LIB/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * LMX_AVR128DB_LIB.cpp
+ *
+ * Created: 26/03/2021 07:36:42
+ * Author : giova
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
